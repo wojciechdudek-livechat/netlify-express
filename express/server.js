3b3af78b8ebfc92c8433992bab984166b9d9f85e
wojciechdudek-livechat <117641044+wojciechdudek-livechat@users.noreply.github.com>
2023-12-01 21:08:23 +0100

    Fixes

--- express/server.js
+++ express/server.js
@@ -1,16 +1,13 @@
 "use strict";
+
 const express = require("express");
-const path = require("path");
 const serverless = require("serverless-http");
+
 const app = express();
-const bodyParser = require("body-parser");
-
 const router = express.Router();
-router.post("/", (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
+router.get("/", (req, res) => res.send("Hello World!"));
 app.use("/.netlify/functions/server", router); // path must route to lambda
-app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
